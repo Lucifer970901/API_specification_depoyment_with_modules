edd222b12d19ed5d7861f29d2ddcb08b4fb9da9d
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-28 20:29:32 +0530

    Update gateway.tf

--- modules/gateway.tf
+++ modules/gateway.tf
@@ -37,3 +37,71 @@ resource "oci_apigateway_gateway" "gateway" {
    
   }
 }
+
+resource "oci_apigateway_deployment" "fieldservice_deployment" {
+    #Required
+    compartment_id =  "${var.compartment_id}"
+    gateway_id =  oci_apigateway_gateway.gateway.id
+    path_prefix = local.api_description.basePath
+    
+    specification {
+
+        #Optional
+        logging_policies {
+
+            #Optional
+            access_log {
+
+                #Optional
+                is_enabled = true
+            }
+            execution_log {
+
+                #Optional
+                is_enabled = true
+                log_level = var.logging_level
+            }
+        }
+        request_policies {
+
+#         rate_limiting {
+#            # Required
+#            rate_in_requests_per_second = "${var.deployment_specification_request_policies_rate_limiting_rate_in_requests_per_second}"
+#            rate_key = "${var.deployment_specification_request_policies_rate_limiting_rate_key}"
+#            }
+        }
+                routes {
+            #Required
+            backend {
+                #Required
+                type = "ORACLE_FUNCTIONS_BACKEND"
+
+                #Optional
+                function_id = "${var.function_id}"
+                connect_timeout_in_seconds = "5"
+                read_timeout_in_seconds = "5"
+                send_timeout_in_seconds = "5"
+            }
+            path = "/pet"
+
+            #Optional
+            logging_policies {
+
+                #Optional
+                access_log {
+
+                    #Optional
+                    is_enabled = true
+                }
+                execution_log {
+
+                    #Optional
+                    is_enabled = true 
+                   #log_level = var.logging_level
+                }
+            }
+            methods = ["GET"]
+        }
+    }
+    display_name = local.api_description.info.title
+  }
