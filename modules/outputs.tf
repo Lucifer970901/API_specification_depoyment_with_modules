7091dc0a91970d036875b61ed6efd95f9dd8b532
Meghashree N <meghashreetendulkar@gmail.com>
2023-02-28 20:17:05 +0530

    Update outputs.tf

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,4 +1,7 @@
-#output "gateway_id_output"{
-#description = "gateway ID"
-#value = oci_apigateway_gateway.gateway.id
-#}
+output "apis" {
+    value = oci_apigateway_api.this.id
+}
+output "gateway_id_output"{
+description = "gateway ID"
+value = oci_apigateway_gateway.gateway.id
+}
