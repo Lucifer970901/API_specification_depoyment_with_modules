b9ebe0575fbdac82650f83b968bf5d75c438282a
Meghashree N <meghashreetendulkar@gmail.com>
2023-03-01 15:57:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "gateway" {
     compartment_id = var.compartment_id
     gateway_endpoint_type = var.gateway_endpoint_type
     subnet_id = var.subnet_id
-   function_id = var.function_id
-   #gwdeploy_params = var.gwdeploy_params
-   # gateway_id = module.gateway.gateway_id_output
+    function_id = var.function_id
+
 }
