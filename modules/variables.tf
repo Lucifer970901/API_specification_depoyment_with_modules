5fb5b7809ce5ed53f985f9f95e8494b6769edfe8
Meghashree N <meghashreetendulkar@gmail.com>
2023-03-01 13:37:38 +0530

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,7 +1,7 @@
 variable "compartment_id"{}
 variable "gateway_endpoint_type" {}
 variable   "subnet_id" {}
-variable "function_ids" {type = map(string)}
+#variable "function_ids" {type = map(string)}
 
 variable "logging_level" {
   default = "ERROR"
