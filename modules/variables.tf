66ff1c6615a02cc66007abc65e3d49ae756d4967
Meghashree N <meghashreetendulkar@gmail.com>
2023-03-01 15:26:22 +0530

    Update variables.tf

--- modules/variables.tf
+++ modules/variables.tf
@@ -2,8 +2,8 @@ variable "compartment_id"{}
 variable "gateway_endpoint_type" {}
 variable   "subnet_id" {}
 
-variable "function_ids" {
-    type = map(string)
+variable "function_id" {
+    type = string
 }
 
 variable "logging_level" {
